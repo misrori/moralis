d6b5fbfec38eccd7884ff611eb586d528bea146a
Mihály Orsós <ormraat.pte@gmail.com>
2024-10-17 20:55:07 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,26 +1,21 @@
-import os
 from scrapethat import *
-from datetime import datetime, timedelta
-
-today = datetime.today()
-today_date_file_name = f"data/life1_class_{today.strftime('%Y-%m-%d')}.csv"
+import requests
+import json
+import datetime
 
 # if data folder not exist create it
 if not os.path.exists("data"):
     os.makedirs("data")
- from scrapethat import *
-import requests
-import json
-import datetime
+
 t = read_cloud('https://moralis.com/api/search/trendingTokens')
 data = json.loads(t.text)
 
 now = datetime.datetime.now()
-day = now.strftime("%y_%m_%d")
-hour = now.strftime("%H_%M")
+mtime = now.strftime("%y_%m_%d_%H_%M")
+
 
 # Create a dynamic filename
-filename = f"data_{day}_{hour}.json"
+filename = f"data/{mtime}.json"
 
 # Save the list to a file
 with open("sample.json", "w") as outfile:
